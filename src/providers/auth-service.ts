3a793cf252a0b8293dddb12793ae1b9e97ebf2b9
magaivermf <magaivermf@gmail.com>
2023-05-01 00:54:26 -0300

    Alteração de nome

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -1,71 +1,30 @@
-import { Http } from '@angular/http';
-
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
-
 import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-
 import { User } from '../models/user';
-
-import { Email } from '../utils/email';
-
 @Injectable()
 export class AuthService {
-  baseApiUrl: string = "http://thinkam.azurewebsites.net/api/";
-
-  constructor(private emailUtils : Email, 
-              private http: Http){
-
-  } 
-
-  currentUser: User;
-
-  public login(credentials) {
-    if (credentials.email === null || credentials.password === null) {
-      return Observable.throw("Por favor, informe suas credenciais");
-    } else {
-      return this.http.post(this.baseApiUrl + "login", credentials)
-                .map(res => res.json())
-                ._catch(error => Observable.throw(error.json()));
-    }
-  }
-
-  public register(credentials) {
-    if (credentials.name === null ||
-        credentials.email === null || 
-        credentials.password === null || 
-        credentials.secretAPIKey === null) {
-      return Observable.throw("Por favor informe todas as informações de registro.");
-    } else {
-      if (credentials.name.length < 3){
-        return Observable.throw("Por favor informe um nome com no mínimo 3 caracteres.");
-      }      
-      if (!this.emailUtils.validateEmail(credentials.email)){
-        return Observable.throw("Por favor informe um e-mail válido.");
-      }      
-      if (credentials.secretAPIKey.length < 8){
-        return Observable.throw("Por favor informe uma secret API key com no mínimo 8 caracteres.");
-      }
-
-      return this.http.post(this.baseApiUrl + "user", credentials)
-                  .map(res => res.json())
-                  ._catch(error => Observable.throw(error.json()));
-    }
-  }
-
-  public getUserInfo(): User {
-    return this.currentUser;
-  }
-
-  public logout() {
-    return Observable.create(observer => {
-      this.currentUser = null;
-      observer.next(true);
-      observer.complete();
-    });
-  }
+currentUser: User;
+public login(credentials) {
+if (credentials.email === null || credentials.password === null) {
+return Observable.throw("Por favor, informe suas credenciais");
+} else {
+return Observable.create(observer => {// At this point make a request to your backend to make a real check!
+let access = (credentials.password === "senha" && credentials.email === "email");
+this.currentUser = new User('Felipe Almeida', 'felipe.almeida@thinkam.net');
+observer.next(access);
+observer.complete();
+});
 }
-
+}
+public getUserInfo(): User {
+return this.currentUser;
+}
+public logout() {
+return Observable.create(observer => {
+this.currentUser = null;
+observer.next(true);
+observer.complete();
+});
+}
+}
